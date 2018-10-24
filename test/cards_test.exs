d89fbc6b61552f6235998bc2b848779ab9726e95
ValdoR Che <ingeekteam@gmail.com>
2018-10-24 20:25:13 +0100

    Running DocTest

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,10 +1,11 @@
 defmodule CardsTest do
   use ExUnit.Case
+  # This line helps to run DOCTEST so every example in documentation with be
+  # run as a test
   doctest Cards
 
   test "contains Ace of Hearts" do
     deck = Cards.create_deck()
-
     assert Cards.contains?(deck, "Ace of Hearts") == true
   end
 end
