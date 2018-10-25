2afa972cbbcf835a3e222a54934b0ac0f7654210
ValdoR Che <ingeekteam@gmail.com>
2018-10-25 13:09:29 +0100

    I worked in CLI with some data structures in Elixir such as Maps ( %{name: ""} ) and keywordLists.

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -8,4 +8,19 @@ defmodule CardsTest do
     deck = Cards.create_deck()
     assert Cards.contains?(deck, "Ace of Hearts") == true
   end
+
+  test "create_deck makes 24 cards" do
+    assert Cards.create_deck() |> Enum.count() == 24
+  end
+
+  # This test is not fair sure because there is a probability to have the shuffle deck in the same order as the original deck
+  test "does shuffle_deck really shuffle the deck __assert_version" do
+    deck = Cards.create_deck()
+    assert deck != Cards.shuffle_deck(deck)
+  end
+
+  test "does shuffle_deck really shuffle the deck __refute_version" do
+    deck = Cards.create_deck()
+    refute deck == Cards.shuffle_deck(deck)
+  end
 end
